e05a1a62d1140e3de3da2a73153de0051e42c1a2
HugoSanche <hbmclaren@gmail.com>
2024-06-05 19:00:09 -0600

    continue validation fields

--- src/main/java/com/individual/controller/IndividualController.java
+++ src/main/java/com/individual/controller/IndividualController.java
@@ -81,16 +81,18 @@ public class IndividualController {
     }
     @PostMapping("/update")
     public String updateIndividual(@Valid @ModelAttribute("individual") Individual theIndividual,
-                                   BindingResult theBindingResult){
+                                   BindingResult theBindingResult, Model model){
 
     //    @{/individuals/showFormForUpdate(theIndividual.personId})
         //Validate if exists errors in thymeleaf
 
-        //showFormForUpdate?individualId=3871
-        //showFormForUpdate?page=3871
-        //showFormForUpdate?individualId=3871
+
+
         if (theBindingResult.hasErrors()){
-            return "individuals/showFormForUpdate?individualId=" + theIndividual.personId;
+            model.addAttribute(theIndividual);
+            /*return "individuals/showFormForUpdate?individualId=" + theIndividual.personId;*/
+
+            return "Update-individual";
         }else{
             //save the individual
             individualService.update(theIndividual);
@@ -98,8 +100,22 @@ public class IndividualController {
             return "redirect:/individuals/list";
         }
 
-    }
 
+    }
+    @PostMapping ("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam ("studentName") String theName, Model model){
+
+        //convert the data to all caps
+        theName=theName.toUpperCase();
+
+        //create the message
+        String result="Bienvenido "+theName;
+
+        //add message to the model
+        model.addAttribute("message",result);
+
+        return "helloword";
+    }
 
     @GetMapping("/delete")
     public String delete(@RequestParam("individualId2") int theId){
